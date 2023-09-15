e85a63d8344a92d6cd8a207bb90fcb9ec2a9aeac
AlejoPaglino <136344335+AlejoPaglino@users.noreply.github.com>
2023-09-15 09:25:18 -0300

    Add files via upload

---
+++ examen alejo paglino/java scrip/alejo.js
@@ -0,0 +1,25 @@
+
+function salario(a)
+{let salarioAnual = 21600
+    let dividirsalario = salarioAnual / 12
+    return dividirsalario
+}
+console.log(salarioAnual(12) )
+
+function salarioSemanal(a)
+{let salarioMensual = 1500
+let multiplicar = salarioMensual * 12
+return multiplicar
+}
+console.log(salarioSemanal(12) )
+
+function promedio (a)
+{let nota1 = a
+let nota2 = b
+ let nota3 = c
+ let promedio = (nota1 + nota2 + nota3) / 3
+return promedio}
+nota1 = prompt ( 'ingrese su nota' )
+nota2 = prompt ( 'ingrese su nota' )
+nota3 = prompt ( 'ingrese su nota' )
+alert ('tu nota es un' + promedio)
